40b01693d44ff51fe5c6fc750f5f656fe5621922
Gerardo Villegas <villegasgerardo.j@gmail.com>
2021-05-08 12:45:20 -0500

    Create commit item design

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -4,5 +4,7 @@ class AppColors {
   static const Color black100 = Color(0xff000000);
   static const Color black70 = Color(0xff131313);
 
+  static const Color grey50 = Color(0xffB3B6B7);
+
   static const Color white100 = Color(0xffFFFFFF);
 }
--- lib/presentation/ui/commit/widgets/commit_item.dart
+++ lib/presentation/ui/commit/widgets/commit_item.dart
@@ -1,4 +1,6 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:githubapp/core/app_colors.dart';
 import 'package:githubapp/core/responsive.dart';
 import 'package:githubapp/domain/models/commit.dart';
 
@@ -9,9 +11,92 @@ class CommitItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final responsive = Responsive.of(context);
     return Container(
+      margin: EdgeInsets.only(bottom: responsive.inchR(2)),
+      padding: EdgeInsets.symmetric(horizontal: responsive.widthR(5)),
       width: double.infinity,
-      child: Row(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: responsive.widthR(65),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  commit.commit.message,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      color: AppColors.white100,
+                      fontWeight: FontWeight.w500,
+                      fontSize: responsive.inchR(2)),
+                ),
+                SizedBox(height: responsive.inchR(.5)),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Container(
+                        height: responsive.inchR(2.5),
+                        width: responsive.inchR(2.5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50)),
+                        child: ExtendedImage.network(
+                          commit.committer.avatarUrl,
+                          fit: BoxFit.cover,
+                          cache: true,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: responsive.inchR(.5)),
+                    Text(commit.committer.login,
+                        style: TextStyle(
+                            color: AppColors.white100,
+                            fontWeight: FontWeight.w500,
+                            fontSize: responsive.inchR(1.7))),
+                    Text(' authored',
+                        style: TextStyle(
+                            color: AppColors.grey50,
+                            fontWeight: FontWeight.w400,
+                            fontSize: responsive.inchR(1.7)))
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: responsive.widthR(20),
+            alignment: Alignment.centerRight,
+            child: Text(
+              dataa(commit.commit.committer.date),
+              maxLines: 1,
+              textAlign: TextAlign.end,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: AppColors.grey50,
+                  fontWeight: FontWeight.w400,
+                  fontSize: responsive.inchR(2)),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  String dataa(DateTime commitDate) {
+    final today = DateTime.now();
+    final different = today.difference(commitDate);
+    final differentHours = different.inHours;
+    if (differentHours <= 0) {
+      return '${different.inMinutes} min';
+    } else if (differentHours <= 24) {
+      return '${different.inHours} h';
+    } else {
+      return '${different.inDays} days';
+    }
   }
 }
